d048a74b3f1baacb9e7c6de1a10905669ce8426a
TegwynTwmffat <paddy@goatindustries.co.uk>
2022-05-28 13:24:01 +0100

    Add files via upload

--- nest_master_satellite_correction/nest_master_satellite_correction.sh
+++ nest_master_satellite_correction/nest_master_satellite_correction.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-# cd /home/pi/Desktop/ROS_startup_scripts/talker_listener_test/ && ./start_master_talker_listener_test.sh
+# cd /home/pi/Desktop/ROS_startup_scripts/nest_master_satellite_correction/ && ./nest_master_satellite_correction.sh
 
-printf "${GREEN}talker_listener_test.${NC}\n" 
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+printf "${GREEN}nest_master_satellite_correction.sh${NC}\n" 
 echo ""
 
 xdotool getactivewindow windowmove 0 100 windowsize 600 370 && lxterminal --command='./terminal_1.sh' &
