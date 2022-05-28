08a21dd8c5129ea6def32e55fa83a01caddf689a
TegwynTwmffat <paddy@goatindustries.co.uk>
2022-05-28 10:09:13 +0100

    Add files via upload

---
+++ nest_slave_satellite_correction.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# cd /home/pi/ && ./nest_slave_satellite_correction.sh
+# A copy of this file must be in the home directory!
+
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+source /home/pi/ros_catkin_ws/devel/setup.bash
+export ROS_MASTER_URI=http://master01:11311
+rosrun talker_listener_test talker.py
