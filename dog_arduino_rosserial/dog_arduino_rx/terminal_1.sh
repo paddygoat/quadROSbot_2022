36067a94c66d19e8971aa2bfa022e89789a26dcf
TegwynTwmffat <paddy@goatindustries.co.uk>
2022-07-15 09:56:06 +0100

    Update terminal_1.sh

--- dog_arduino_rosserial/dog_arduino_rx/terminal_1.sh
+++ dog_arduino_rosserial/dog_arduino_rx/terminal_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /home/pi/ros_catkin_ws/devel/setup.bash && rosrun rosserial_python serial_node.py __name:="toggle_LED" _port:=/dev/ttyACM0
+source /home/pi/ros_catkin_ws/devel/setup.bash && rosrun rosserial_python serial_node.py __name:="toggle_LED" _port:=/dev/ttyUSB0
 
 #  _baud:=115200
 
