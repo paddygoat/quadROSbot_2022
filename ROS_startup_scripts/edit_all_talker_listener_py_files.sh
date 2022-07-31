1cfecdd63de40336698aa0b2bef35b8cb02f71c7
TegwynTwmffat <paddy@goatindustries.co.uk>
2022-07-31 09:53:29 +0100

    Add files via upload

--- ROS_startup_scripts/edit_all_talker_listener_py_files.sh
+++ ROS_startup_scripts/edit_all_talker_listener_py_files.sh
@@ -8,8 +8,10 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 
-printf "${GREEN}edit_nest_master_talker_listener_py ....${NC}\n"
+printf "${GREEN}edit master_talker_listener_py files ....${NC}\n"
+printf "${GREEN}and copy those files to /home/paddy/copy/ ....${NC}\n"
 echo ""
+
 sleep 1
 
 gnome-terminal --geometry=95x60-1200+0 -- bash -c " \
@@ -42,8 +44,13 @@ sshpass -p "whales" ssh -t pi@192.168.1.236 -o StrictHostKeyChecking=no \
 nano dog_slave_talker_listener.py' \
 ; exec bash"
 
+sshpass -p "whales" scp pi@192.168.1.134:/home/pi/ros_catkin_ws/src/nest_master_satellite_correction_package/src/nest_master_talker_listener.py /home/paddy/copy/.
+sshpass -p "whales" scp pi@192.168.1.200:/home/pi/ros_catkin_ws/src/nest_slave_satellite_correction_package/src/nest_slave_talker_listener.py /home/paddy/copy/.
 
+sshpass -p "whales" scp pi@192.168.1.161:/home/pi/ros_catkin_ws/src/dog_master_satellite_correction_package/src/dog_master_talker_listener.py /home/paddy/copy/.
+sshpass -p "whales" scp pi@192.168.1.236:/home/pi/ros_catkin_ws/src/dog_slave_satellite_correction_package/src/dog_slave_talker_listener.py /home/paddy/copy/.
 
+# sshpass -p "whales" scp -r pi@192.168.1.236:/home/pi/ros_catkin_ws/src/* /home/paddy/copy/.
 
 
 
